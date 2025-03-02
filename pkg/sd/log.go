ac57ae909ff3a1cae82efce6805a6057fbfbb64c
Binozo <entwickler@binozoworks.de>
2025-03-02 16:28:06 +0100

    Added log/progress callbacks

--- pkg/sd/log.go
+++ pkg/sd/log.go
@@ -1,10 +1,27 @@
 package sd
 
-import "GoStableDiffusion/internal/sd"
+import (
+	"GoStableDiffusion/internal/sd"
+	"unsafe"
+)
+
+type LogLevel sd.LogLevel
 
 const (
-	LogDebug sd.LogLevel = sd.LogDebug
-	LogInfo  sd.LogLevel = sd.LogInfo
-	LogWarn  sd.LogLevel = sd.LogWarn
-	LogError sd.LogLevel = sd.LogError
+	LogDebug LogLevel = LogLevel(sd.LogDebug)
+	LogInfo  LogLevel = LogLevel(sd.LogInfo)
+	LogWarn  LogLevel = LogLevel(sd.LogWarn)
+	LogError LogLevel = LogLevel(sd.LogError)
 )
+
+type LogCallback func(level LogLevel, text string, data unsafe.Pointer)
+
+func SetLogCallback(callback LogCallback) {
+	sd.SetLogCallback(func(level sd.LogLevel, text string, data unsafe.Pointer) {
+		callback(LogLevel(level), text, data)
+	})
+}
+
+func SetProgressCallback(callback sd.ProgressCallback) {
+	sd.SetProgressCallback(callback)
+}
