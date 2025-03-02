38f12341b27ea16b9be82f12602c8b3d9a52fa78
Binozo <entwickler@binozoworks.de>
2025-03-02 16:28:28 +0100

    Fixed linking issue

---
+++ pkg/sd/c.go
@@ -0,0 +1,15 @@
+package sd
+
+// This is really weird, somehow I get linking errors if I remove this here. This is the same as in internal/sd/c.go
+
+/*
+#cgo CFLAGS: -I../../stable-diffusion.cpp/
+#cgo LDFLAGS: -static -L../../stable-diffusion.cpp/build/ggml/src -lggml -lggml-cpu -lggml-base
+#cgo LDFLAGS: -lstdc++ -lm -fopenmp -L../../stable-diffusion.cpp/build -lstable-diffusion
+#include "../../stable-diffusion.cpp/stable-diffusion.h"
+*/
+import "C"
+
+func init() {
+
+}
