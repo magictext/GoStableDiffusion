d300c41df6f61854c9361a4c0eebd9444084012e
Binozo <entwickler@binozoworks.de>
2025-03-23 19:06:06 +0100

    trying to fix windows build

--- build.bat
+++ build.bat
@@ -29,5 +29,6 @@ if "%VULKAN%"=="1" (
 cmake .. -DSD_CUDA=%SD_CUDA% -DSD_METAL=%SD_METAL% -DSD_VULKAN=%SD_VULKAN% -DSD_BUILD_SHARED_LIBS=ON
 
 cmake --build . --config Release -j%NUMBER_OF_PROCESSORS%
-dir /s /b *.dll
 copy bin\Release\stable-diffusion.dll ..\..
+copy bin\Release\stable-diffusion.dll bin
+dir /s /b *.dll
